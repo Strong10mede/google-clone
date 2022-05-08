b0121505fb8c005ab5505653b3e83a96435232b7
Strong10mede <kmayur809@gmail.com>
2022-05-08 17:38:51 +0530

    commit

--- src/components/search.js
+++ src/components/search.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './search.css'
 import SearchIcon from '@mui/icons-material/Search';
 import MicIcon from '@mui/icons-material/Mic';
-
+import Button from '@mui/material/Button'
 function search() {
   return (
     <div className='search'>
@@ -10,6 +10,11 @@ function search() {
             <SearchIcon className='search__inputIcon'/>
             <input />
             <MicIcon />
+        </div>
+        <div className='search__buttons'>
+            <Button variant='outlined'>Google Search</Button>
+            {/* variabt is material ui prop */}
+            <Button variant='outlined'>I'm Feeling Lucky</Button>
         </div>
     </div>
   )
