da9cfa9e82215c702cf19f215d5cc9bb0c72ffba
Strong10mede <kmayur809@gmail.com>
2022-05-26 21:16:38 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <StateProvider initialState={initialState} reducer={reducer}>
     <App />
+    </StateProvider>
   </React.StrictMode>
 );
 
