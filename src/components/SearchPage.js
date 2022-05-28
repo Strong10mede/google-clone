be3ea78ee0d917d98872a5d549b2626f51cdbe4e
Strong10mede <kmayur809@gmail.com>
2022-05-28 09:10:19 +0530

    commit

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useStateValue } from '../StateProvider'
 import './SearchPage.css'
 function SearchPage() {
+    const [{term},dispatch] =useStateValue();
+
   return (
     <div className='searchPage'>
-        <div className='searchPage__header'></div>
+        <div className='searchPage__header'>
+            <h1>{term}</h1>
+        </div>
         <div className='searchPage__result'></div>
     </div>
   )
