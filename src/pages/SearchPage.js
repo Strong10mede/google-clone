3942d191c8cee3ed3510fb1afa0b67e06fd0f249
Strong10mede <kmayur809@gmail.com>
2022-05-29 20:12:38 +0530

    commit

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './SearchPage.css'
 import { useStateValue } from '../StateProvider/StateProvider'
+import useSearchPage from '../useSearchPage';
 function SearchPage() {
     // eslint-disable-next-line no-unused-vars
     const [{term} , dispatch] = useStateValue();
-    console.log(term);
+    const [data] = useSearchPage(term);
+    console.log(data);
 
     //https://developer.google.com/custom-search/v1/using_rest  custom search api key
     //https://cse.google.com/cse/create/new  custom hook
