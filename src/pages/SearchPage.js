208f43d2f10212578f34af17601f17c454f4bde0
Strong10mede <kmayur809@gmail.com>
2022-05-30 17:50:41 +0530

    commit

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,34 +1,37 @@
-import React from 'react'
-import './SearchPage.css'
-import { useStateValue } from '../StateProvider/StateProvider'
-import useSearchPage from '../useSearchPage';
-import Response from '../response';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./SearchPage.css";
+import { useStateValue } from "../StateProvider/StateProvider";
+import useSearchPage from "../useSearchPage";
+import Response from "../response";
+import { Link } from "react-router-dom";
+import "./SearchPage.css";
 function SearchPage() {
-    // eslint-disable-next-line 
-    const [{term} , dispatch] = useStateValue();
+  // eslint-disable-next-line
+  const [{ term }, dispatch] = useStateValue();
 
-    // LIVE API_CALL
-    //const {data} = useSearchPage(term);
-    
-     
-    const data = Response;
-    //https://developer.google.com/custom-search/v1/using_rest  custom search api key
-    //https://cse.google.com/cse/create/new  custom hook
+  // LIVE API_CALL
+  //const {data} = useSearchPage(term);
 
-    console.log(data);
+  const data = Response;
+  //https://developer.google.com/custom-search/v1/using_rest  custom search api key
+  //https://cse.google.com/cse/create/new  custom hook
+
+  console.log(data);
 
   return (
-    <div className='searchPage'>
-        <div className='searchPage__header'>
-            <Link to='/'>
-              <img className='searchPage__logo' src='https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png' alt='google_logo'/>
-            </Link>
-            
-        </div>
-        <div className='searchPage__result'></div>
+    <div className="searchPage">
+      <div className="searchPage__header">
+        <Link to="/">
+          <img
+            className="searchPage__logo"
+            src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+            alt="google_logo"
+          />
+        </Link>
+      </div>
+      <div className="searchPage__result"></div>
     </div>
-  )
+  );
 }
 
-export default SearchPage
+export default SearchPage;
