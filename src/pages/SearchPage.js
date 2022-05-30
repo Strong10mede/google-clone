ab10cea9b82800eaab8712e9f3d56c26092edce3
Strong10mede <kmayur809@gmail.com>
2022-05-30 18:51:58 +0530

    commit

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -37,40 +37,41 @@ function SearchPage() {
         </Link>
         <div className="searchPage__headerBody">
           <Search hideButtons />
-        </div>
-        <div className="searchPage__options">
-          <div className="searchPage__optionsLeft">
-            <div className="searchPage__option">
-              <SearchIcon />
-              <Link to="/all">All</Link>
+
+          <div className="searchPage__options">
+            <div className="searchPage__optionsLeft">
+              <div className="searchPage__option">
+                <SearchIcon />
+                <Link to="/all">All</Link>
+              </div>
+              <div className="searchPage__option">
+                <DescriptionIcon />
+                <Link to="/news">News</Link>
+              </div>
+              <div className="searchPage__option">
+                <ImageIcon />
+                <Link to="/images">Images</Link>
+              </div>
+              <div className="searchPage__option">
+                <LocalOfferIcon />
+                <Link to="/shopping">Shopping</Link>
+              </div>
+              <div className="searchPage__option">
+                <RoomIcon />
+                <Link to="/maps">Maps</Link>
+              </div>
+              <div className="searchPage__option">
+                <MoreVertIcon />
+                <Link to="/more">More</Link>
+              </div>
             </div>
-            <div className="searchPage__option">
-              <DescriptionIcon />
-              <Link to="/news">News</Link>
-            </div>
-            <div className="searchPage__option">
-              <ImageIcon />
-              <Link to="/images">Images</Link>
-            </div>
-            <div className="searchPage__option">
-              <LocalOfferIcon />
-              <Link to="/shopping">Shopping</Link>
-            </div>
-            <div className="searchPage__option">
-              <RoomIcon />
-              <Link to="/maps">Maps</Link>
-            </div>
-            <div className="searchPage__option">
-              <MoreVertIcon />
-              <Link to="/more">More</Link>
-            </div>
-          </div>
-          <div className="searchPage__optionsRight">
-            <div className="searchPage__option">
-              <Link to="/settings">Settings</Link>
-            </div>
-            <div className="searchPage__option">
-              <Link to="/tools">Tools</Link>
+            <div className="searchPage__optionsRight">
+              <div className="searchPage__option">
+                <Link to="/settings">Settings</Link>
+              </div>
+              <div className="searchPage__option">
+                <Link to="/tools">Tools</Link>
+              </div>
             </div>
           </div>
         </div>
